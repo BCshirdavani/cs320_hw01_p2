03e902fcb1376970c4e624250ea2de4cff01b74b
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-01-23 22:24:12 -0800

    Available Quarters Checked
    I can now see list of available quarters....
    
    To-Do:
    -Make Menu to send new URL request based on selected quarter.
    -part 2: Menu to ask for programs, and display all with chosen starting letter....then do another reg exp to grab the final schedule...

--- src/Schedule.java
+++ src/Schedule.java
@@ -20,6 +20,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,7 +46,7 @@ public class Schedule {
         String text;
 
         text = URLget.URLcontent(page);
-        System.out.println(text);
+//        System.out.println(text);
 
 //        Pattern pattern = Pattern.compile("<td>(.*)\\s*</td>\\s*<td>(.*)\\s*</td>\\s*<td>(.*)\\s*</td>");
 //        Matcher matcher = pattern.matcher(text);
@@ -55,6 +56,33 @@ public class Schedule {
 //            System.out.println("Exam Time: " + matcher.group(3));
 //            System.out.println("++++++++++++++++++++");
 //        }
+
+
+        // Grab the Quarters available on the website
+        Pattern pattern = Pattern.compile("<a\\sclass=.*/classes(.*)\">.*</a>");
+        Matcher matcher = pattern.matcher(text);
+        int matchCounter = 0;
+        while(matcher.find()){
+            System.out.println(matcher.group(1));
+            matchCounter++;
+        }
+        System.out.println("match count: " + matchCounter);
+
+        // Now grab the available quarters and put into a string array list
+        Pattern pattern2 = Pattern.compile("<a\\sclass=.*/classes(.*)\">.*</a>");
+        Matcher matcher2 = pattern2.matcher(text);
+        String[] quarters = new String[matchCounter];
+        int quarterINDEX = 0;
+        while(matcher2.find()){
+            System.out.println("quarter indes: " + quarterINDEX);
+            quarters[quarterINDEX] = matcher2.group(1);
+            quarterINDEX++;
+        }
+        System.out.println(Arrays.toString(quarters));
+
+
+
+
 
 
     }

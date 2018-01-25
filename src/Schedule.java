e2dd66898f0a005d18992c7ea25950c221feebca
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-01-24 18:29:51 -0800

    goes to program page
    to-do:
    make final reg exp to grab the schedule from this program page.

--- src/Schedule.java
+++ src/Schedule.java
@@ -37,15 +37,6 @@ public class Schedule {
         text = URLget.URLcontent(page);
 //        System.out.println(text);
 
-//        Pattern pattern = Pattern.compile("<td>(.*)\\s*</td>\\s*<td>(.*)\\s*</td>\\s*<td>(.*)\\s*</td>");
-//        Matcher matcher = pattern.matcher(text);
-//        while(matcher.find()){
-//            System.out.println("Class Time: " + matcher.group(1));
-//            System.out.println("Exam Day: " + matcher.group(2));
-//            System.out.println("Exam Time: " + matcher.group(3));
-//            System.out.println("++++++++++++++++++++");
-//        }
-
 
         // Grab the Quarters available on the website
         Pattern pattern = Pattern.compile("<a\\sclass=.*/classes(.*)\">.*</a>");
@@ -57,22 +48,20 @@ public class Schedule {
         }
         System.out.println("match count: " + matchCounter);
 
+
         // Now grab the available quarters and put into a string array list
         Pattern pattern2 = Pattern.compile("<a\\sclass=.*/classes(.*)\">.*</a>");
         Matcher matcher2 = pattern2.matcher(text);
         String[] quarters = new String[matchCounter];
         int quarterINDEX = 0;
         while(matcher2.find()){
-            System.out.println("quarter indes: " + quarterINDEX);
+            System.out.println("quarter index: " + quarterINDEX);
             quarters[quarterINDEX] = matcher2.group(1);
             quarterINDEX++;
         }
 
-
-
         System.out.println("Available quarter + Year combos listed on page:");
         System.out.println(Arrays.toString(quarters));
-
 
 
         //  MAKE MENU HERE
@@ -89,6 +78,11 @@ public class Schedule {
             System.out.println("Enter a year: ");
             choiceYR = scIN.nextLine();
         }
+        String choiceLETTER = null;
+        while (choiceLETTER == null){
+            System.out.println("Enter the initial for the program: ");
+            choiceLETTER = scIN.nextLine();
+        }
 
         String qtrYEAR = choiceQTR + choiceYR;
         System.out.println(qtrYEAR);
@@ -97,37 +91,79 @@ public class Schedule {
         textQTR = URLget.URLcontent(URLquarter);
 
         System.out.println("textQTR length: " + textQTR.length());
-        System.out.println(textQTR.toString());
+//        System.out.println(textQTR.toString());
+
 
         //============================================================  ++
+        //  this will concatenate the regular expression
+        //        Pattern pattern3 = Pattern.compile("<a\\shref=.+/classes/.+>(A.+)</a>");  // starts with "A"
+        String regExpSearch_3;
+        String part1 = "<a\\shref=.+/classes/.+>(";
+        String part3 = ".+)</a>";
+        regExpSearch_3 = part1 + choiceLETTER + part3;
+
         //  list all programs that start with a letter "A"
-        Pattern pattern3 = Pattern.compile("href=\"/classes/Spring2018/A\\w*\"(A.*)</a>");  // starts with "A"
-//        Pattern pattern3 = Pattern.compile("<h1.*pageTitle(\\w*)</h1>");
+        //"<a\\shref=.+/classes/.+>(A.+)</a>"
+//        Pattern pattern3 = Pattern.compile("<a\\shref=.+/classes/.+>(A.+)</a>");  // starts with "A"
+
+        //  this will search based on the letter chosen earlier
+        Pattern pattern3 = Pattern.compile(regExpSearch_3);
+
+
         Matcher matcher3 = pattern3.matcher(textQTR);
         int progINDEX = 0;
         while(matcher3.find()){
-            System.out.println("program index: " + progINDEX);
-//            System.out.println(matcher3.group(0));
             System.out.println(matcher3.group(1));
-//            quarters[progINDEX] = matcher3.group(0);
-//            quarters[progINDEX] = matcher3.group(1);
             progINDEX++;
         }
+        System.out.println("progINDEX = " + progINDEX);
         //============================================================  ++
 
-//        <a href="/classes/Spring2018/ACCT">Accounting</a>
 
 
-        
+
 
         //  MAKE MENU AGAIN
         //      choose program
         //      choose class
         //          send new string, concat URL, to get new website text
         //          make new reg exp, to grab the schedule
+        String choicePROG = null;
+        while (choicePROG == null){
+            System.out.println("Enter the program's name: ");
+            choicePROG = scIN.nextLine();
+        }
+        String choiceCID = null;
+        while (choiceCID == null){
+            System.out.println("Enter the course ID: ");
+            choiceCID = scIN.nextLine();
+        }
+        //  use reg exp to grab the URL associated with the program name
+        String regExpSearch_4;
+//        regExpSearch_4 = "<a\\shref=.(.+).>" + choicePROG + "</a>";
+        regExpSearch_4 = "<a\\shref=./classes(.+).>" + choicePROG + "</a>";
+
+        Pattern pattern4 = Pattern.compile(regExpSearch_4);
+        Matcher matcher4 = pattern4.matcher(textQTR);
+        String URLclass ="";
+        String classURLext;
+        int urlINDEX = 0;
+        while(matcher4.find()){
+            System.out.println("matching URL extension is: " + matcher4.group(1));
+            classURLext = matcher4.group(1);
+            System.out.println("URL of page is: " + page);
+            URLclass = page + classURLext;
+            System.out.println("Program URL is: " + URLclass);
+            urlINDEX++;
+        }
+
+        text = URLget.URLcontent(URLclass);
+
+        System.out.println("FINAL PAGE: \n\n" + text);
 
 
-
+        String regExpSearch_5;
+        
 
 
     }
